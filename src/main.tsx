6809e6d15afed60615d54cfb863c72a052b06626
Samitha <samitha@Samitha-MacBook-Ca.local>
2025-01-01 10:10:28 +0530

    feat: main ts and css file were modified to include necessary details only

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@fontsource/source-sans-pro";
+import "./index.css";
+import App from "./App.js";
+
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Container not found");
+}
+
+const root = createRoot(container);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
